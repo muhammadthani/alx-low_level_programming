4608a1db0d30ae56e8dd96dce4e9a8dc41ec65ec
muhammadthani <thaniabdulkadir@gmail.com>
2022-09-08 06:13:48 -0700

    6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main (){
-	char sentence[] = "Programming is like building a multilingual puzzle";
+int main(void) {
+	char sentence[] = "\"Programming is like building a multilingual puzzle";
 	puts(sentence);
 	return 0;
 }

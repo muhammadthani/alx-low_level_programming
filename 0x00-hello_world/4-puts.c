992e0c0932178a9854289d313bbd41f00ec3d8f2
muhammadthani <thaniabdulkadir@gmail.com>
2022-09-08 06:33:30 -0700

    9

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
+/*This is a simple C program*/
 #include <stdio.h>
-/*This is a c program that prints a sentence*/
 int main(void) 
 {
 	char sentence[] = "\"Programming is like building a multilingual puzzle";

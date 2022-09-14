5e8949becffd91df5ea664056ec581fa96ac1cd7
muhammadthani <thaniabdulkadir@gmail.com>
2022-09-13 18:06:35 -0700

    NestedLoops4

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,17 @@
 #include "main.h"
+
 /**
- * print_alphabet - function
- * Return: done
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
  */
 void print_alphabet(void)
 {
-	char low = 'a';
+	char x;
 
-	for (low = 'a'; low <= 'z'; low++)
-		_putchar(low);
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
 	_putchar('\n');
 }
-

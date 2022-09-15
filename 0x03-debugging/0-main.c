86b16639ff077add316e33bac98d8a90c0cd76bd
muhammadthani <thaniabdulkadir@gmail.com>
2022-09-15 04:53:56 -0700

    debug1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - defines what the main purpose is
+ * here is another line of command
+ * Return: gives a response
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}

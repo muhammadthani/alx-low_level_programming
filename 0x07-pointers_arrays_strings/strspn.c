611b7ce8fd76fd5d506e811cbabf3b612055024b
muhammadthani <thaniabdulkadir@gmail.com>
2022-09-26 01:39:50 -0700

    Arrays1

---
+++ 0x07-pointers_arrays_strings/strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strspn - main
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
+			break;
+	}
+	return (i);
+}

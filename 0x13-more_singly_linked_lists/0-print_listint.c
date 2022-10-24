3d633506fdd689867899615227562853ed247510
muhammadthani <thaniabdulkadir@gmail.com>
2022-10-24 10:21:06 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint - print the list of integers
+ * @h: head of the list
+ * Return: returns null
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor != NULL)
+	{
+		printf("%d\n", cursor->n);
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
+}
